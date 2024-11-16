1b49f0fb2a1452cb6f444a9911cf1c588745549b
jMiers1 <julius.miers@gmail.com>
2024-11-16 19:34:03 +0100

    start pc tsp model

--- src/_own_KidneyModel.cpp
+++ src/_own_KidneyModel.cpp
@@ -5,6 +5,7 @@
 
 #include <vector>
 #include <map>
+#include <utilit>
 
 
 // Constructor definition
@@ -16,18 +17,84 @@ KidneyModel::KidneyModel(IloEnv& _env,
                         const map<int,double>& _chainWeights,
                         const map<int,double>& _cycleWeights,
                         const vector<int>& _ndds,
-                        const vector<int>& _pdps)
-    : env(_env), _Cycles(_cycles), _Chains(_chains), _cycleWeights(_cycleWeights), _chainWeights(_chainWeights), _Weights(_weights), _mapNodes(_mapNodes), _NDDs(_ndds), _PDPs(_pdps){
+                        const vector<int>& _pdps,
+                        const vector<vector<int>>& adjacencyList,  
+                        const vector<vector<int>>& predList)
+    : env(_env), _Cycles(_cycles), _Chains(_chains), _cycleWeights(_cycleWeights), _chainWeights(_chainWeights), _Weights(_weights), _mapNodes(_mapNodes), _NDDs(_ndds), _PDPs(_pdps), _AdjacencyList(adjacencyList), _PredList(predList) {
         _numCycles = _Cycles.size();
         _numChains = _Chains.size();
         solvePatternFormulation();
+
+        _numEdges = _Weights.size();
+        _numNodes = _NNDs.size() + _PDPs.size();
     }
 
-KidneyModel::~KidneyModel() {
-    env.end();  
+double KidneyModel::solvePctspFormulation(){
+    double result;
+
+    // Decision variables
+    IloBoolVarArray x_cycle(env, _numCycles);  
+    map<pair<int, int>, IloBoolVar> x_edge;
+    IloBoolVarArray f_out(env, _numNodes);  
+    IloBoolVarArray f_in(env, _numNodes);  
+
+    // Objective
+    IloExpr obj(env);
+    for (int i = 0; i < _numCycles; ++i) {
+        double cycle_weight = _cycleWeights[i]; 
+        obj += cycle_weight * x_cycle[i];  
+    }
+
+    for (const auto& entry : _Weights) {
+        const pair<int, int>& edge = entry.first;  // Edge (i, j)
+        double edge_weight = entry.second;         // Weight of the edge
+        x_edge[edge] = IloBoolVar(env);        // Create IloBoolVar for the edge
+        obj += edge_weight * x_edge[edge];
+    }
+    IloObjective objective = IloMaximize(env, obj);
+
+    this -> printObjectiveFunction();
+
+
+    // Constraints: Each node can appear at most once
+    IloRangeArray constraints(env);
+    for (int i = 0; i < _numNodes; i++){
+        IloExpr out_Constraint(env);
+        for (int predecessor : _PredList[i]){
+            pair<int,int> edge = {predecessor, i};
+            out_Constraint += x_edge[edge]
+        }
+        
+        constraints.add(f_out[i] == out_Constraint);
+
+
+
+
+
+    }
+    for (auto& node_pair : _mapNodes) {
+        int node = node_pair.first;
+        vector<int> node_chains = node_pair.second.first; // a list of the chains containing the node
+        vector<int> node_cycles = node_pair.second.second; // a list of the cycles containing the node   
+
+        // total sum of chaining cycles and chains <= 1
+        IloExpr nodeConstraint(env);
+        for (int cycle_index : node_cycles) {
+            nodeConstraint += x_cycle[cycle_index];
+        }
+        for (int chain_index : node_chains) {
+            nodeConstraint += x_chain[chain_index];
+        }
+        constraints.add(IloRange(env, 0, nodeConstraint, 1)); 
+        nodeConstraint.end(); 
+
+        // cout << "node " << node << " chains " << node_chains.size()<< " cycles " << node_cycles.size() <<endl;
+    }
+
+
+
+
 }
-
-
 double KidneyModel::solvePatternFormulation() {
     double result;
 
@@ -47,15 +114,9 @@ double KidneyModel::solvePatternFormulation() {
     }
     IloObjective objective = IloMaximize(env, obj);
 
-    // Print the objective function
-    // cout << "Objective function: " << endl;
-    // for (int i = 0; i < _numCycles; ++i) {
-    //     cout << " + " << _cycleWeights[i] << " * x_cycle[" << i << "]";
-    // }
-    // for (int i = 0; i < _numChains; ++i) {
-    //     cout << " + " << _chainWeights[i] << " * x_path[" << i << "]";
-    // }
-    // cout << endl;
+    this -> printObjectiveFunction();
+
+    
 
 
     // Constraints: Each node can appear at most once
@@ -100,8 +161,21 @@ double KidneyModel::solvePatternFormulation() {
         cout << "ERROR" << endl;
         return 0;
     }
+    env.end();
 }
 
+
+void KidneyModel::printObjectiveFunction(){
+  
+    cout << "Objective function: " << endl;
+    for (int i = 0; i < _numCycles; ++i) {
+        cout << " + " << _cycleWeights[i] << " * x_cycle[" << i << "]";
+    }
+    for (int i = 0; i < _numChains; ++i) {
+        cout << " + " << _chainWeights[i] << " * x_path[" << i << "]";
+    }
+    cout << endl;
+}
 
 void KidneyModel::printSolution(const double& obj, const IloCplex& cplex, const IloBoolVarArray& x_cycle, const IloBoolVarArray& x_chain){
 
--- src/_own_KidneyModel.hpp
+++ src/_own_KidneyModel.hpp
@@ -18,12 +18,15 @@ public:
                 const map<int,double>& _chainWeights,
                 const map<int,double>& _cycleWeights,
                 const vector<int>& _ndds,
-                const vector<int>& _pdps);
-    ~KidneyModel();
+                const vector<int>& _pdps,
+                const vector<vector<int>>& adjacencyList,  
+                const vector<vector<int>>& predList);
     double solvePatternFormulation();
     void printSolutionStructure(const string& structure_type, const int& structure);
-    void printSolution(const string& structure_type, const int& structure);
     void printSolution(const double& obj, const IloCplex& cplex, const IloBoolVarArray& x_cycle, const IloBoolVarArray& x_chain);
+    void printObjectiveFunction();
+
+    void solvePctspFormulation();
 
 private:
     IloEnv env;
@@ -35,8 +38,12 @@ private:
     map<int,pair<vector<int>,vector<int>>> _mapNodes;
     vector<int> _NDDs;
     vector<int> _PDPs;
-    int _numCycles{-1};
-    int _numChains{-1};
+    int _numCycles{-99};
+    int _numChains{-99};
+    int _numEdges{-99};
+    int _numNodes{-99};
+    vector<vector<int>> _AdjacencyList;
+    vector<vector<int>> _PredList;
 };
 
 #endif // KIDNEYEXCHANGE_HPP
